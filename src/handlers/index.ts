77065636da0f4c9da00ef3eb88e1da68df3d2e5b
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-12-18 17:19:31 -0300

    use cloudinary para cargar la imagen de perfil

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,12 @@
 import type { Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 import slug from 'slug';
+import formidable from 'formidable'
+import { v4 as uuid } from 'uuid'
 import User from "../models/User"
 import { checkPassword, hashPassword } from '../utils/auth';
 import { generateJWT } from '../utils/jwt';
+import cloudinary from '../config/cloudinary';
 
 
 export const createAccount = async (req: Request, res: Response) => {
@@ -83,6 +86,31 @@ export const updateProfile = async (req: Request, res: Response) => {
         await req.user.save();
         res.send('Perfil actualizado correctamente')
 
+    } catch (e) {
+        const error = new Error('Hubo un error')
+        return res.status(500).json({error: error.message})
+    }
+}
+
+export const uploadImage = async (req: Request, res: Response) => {
+    
+    const form = formidable({multiples: false})
+    try {
+        form.parse(req, (error, fields, files) => {
+            console.log(files)
+
+            cloudinary.uploader.upload(files.file[0].filepath, {public_id: uuid()}, async function (error, result) {
+                if (error) {
+                    const error = new Error('Hubo un error al subir la imagen')
+                    return res.status(500).json({error: error.message})
+                }
+                if (result) {
+                    req.user.image = result.secure_url;
+                    await req.user.save();
+                    res.json({image: result.secure_url})
+                }
+            } )
+        })
     } catch (e) {
         const error = new Error('Hubo un error')
         return res.status(500).json({error: error.message})
